a20e3575b17dd954a26af5e1f674881182bc99b0
James Stone <jamesmstone@users.noreply.github.com>
2021-02-25 20:22:23 +1100

    Unlock keybase

--- postLogin/keybase.sh
+++ postLogin/keybase.sh
@@ -2,3 +2,6 @@
 set -euo pipefail
 source ~/.dockerfunc
 lpass show "keybase paperkey" --notes | keybase oneshot -u jamesstone;
+lpass show keybase.io --pass |
+    head -c -2 | # this is needed as lastpass adds two '\n' at the end of the password.
+    keybase unlock --stdin;                                                               
